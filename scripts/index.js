8271e80fffefea524a4a6b1a24243e7945e81e56
Olga Maykova <dolkaaaaa@gmail.com>
2022-01-18 21:26:16 +0300

    feat: отработана popup

--- scripts/index.js
+++ scripts/index.js
@@ -6,20 +6,23 @@ const profileName = document.querySelector('.profile__info-name');
 const popupName = document.querySelector('.popup__name');
 const profileDescription = document.querySelector('.profile__info-description');
 const popupDescription = document.querySelector('.popup__description');
+const form = document.querySelector('.popup__container');
 
 editProfileDescriptionButton.addEventListener('click', function () {
-  popup.classList.remove('popup__hidden');
   popup.classList.add('popup__opened');
+  popupName.setAttribute('value', profileName.textContent);
+  popupDescription.setAttribute('value', profileDescription.textContent);
 });
 
 popupCloseButton.addEventListener('click', function() {
+  event.preventDefault();
   popup.classList.remove('popup__opened');
-  popup.classList.add('popup__hidden');
+  form.reset();
 });
 
-popupSaveButton.addEventListener('click', function() {
-  profileName.textContent = popupName.textContent;
-  profileDescription.textContent = popupDescription.textContent;
+form.addEventListener('submit', function() {
+  event.preventDefault();
+  profileName.textContent = popupName.value;
+  profileDescription.textContent = popupDescription.value;
   popup.classList.remove('popup__opened');
-  popup.classList.add('popup__hidden');
-});
+})
