186e361af41b2b91ad46c64b53d2af1100d59cf3
Olga Maykova <dolkaaaaa@gmail.com>
2022-01-28 12:03:58 +0300

    style: поправлены стили после ревью2

--- scripts/index.js
+++ scripts/index.js
@@ -7,12 +7,12 @@ const profileDescription = document.querySelector('.profile__info-description');
 const popupDescription = document.querySelector('.popup__input_type_description');
 const form = document.querySelector('.popup__edit-form');
 function openPopup () {
-  popup.classList.add('popup_display_opened');
+  popup.classList.add('popup_opened');
   popupName.value = profileName.textContent;
   popupDescription.value = profileDescription.textContent;
 };
 function closePopup() {
-  popup.classList.remove('popup_display_opened');
+  popup.classList.remove('popup_opened');
 }
 function savePopup() {
   event.preventDefault();
