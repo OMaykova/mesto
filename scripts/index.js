63eea1f41750d9cea3c64a6466599c4faf91358f
Olga Maykova <dolkaaaaa@gmail.com>
2022-02-04 20:10:41 +0300

    style: изменить элементы массива карточек

--- scripts/index.js
+++ scripts/index.js
@@ -16,12 +16,12 @@ const popupCardImage = document.querySelector('.popup__card-image');
 const popupCardTitle = document.querySelector('.popup__card-title');
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: 'Волгодонск',
+    link: 'https://kartarf.ru/images/heritage/1080/0/6203.jpg'
   },
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: 'Томск',
+    link: 'https://cont.ws/uploads/pic/2019/6/%D0%A5%D0%B0%D1%84%D0%B8%D0%B7%D0%BE%D0%B2%205%20%281%29.jpg'
   },
   {
     name: 'Иваново',
